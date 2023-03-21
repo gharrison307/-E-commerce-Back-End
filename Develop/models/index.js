ad921b8d3773256dcc906c751cddc2cd77b68bc1
George Harrison <g.a.harrison91@gmail.com>
2023-03-21 08:24:51 -0600

    updated models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,14 +1,25 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
 
